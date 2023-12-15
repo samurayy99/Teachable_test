138029b8f945d5d07a25fd1685636deaef133678
samurayy99 <lenox2779@hotmail.com>
2023-12-15 15:05:44 +0100

    Update17

--- main_teachable.py
+++ main_teachable.py
@@ -1,6 +1,10 @@
 import os
 import sys
 import pickle
+import asyncio
+import logging
+import spacy
+import chromadb
 from typing import Callable, Dict, Optional, Union, List, Tuple, Any
 from termcolor import colored
 from tqdm import tqdm
@@ -9,43 +13,39 @@ from autogen.agentchat.contrib.text_analyzer_agent import TextAnalyzerAgent
 from autogen.agentchat.agent import Agent
 from autogen import UserProxyAgent
 from chromadb.config import Settings
-import chromadb
 
-filter_dict = {"model": ["gpt-4"]}
-config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST", filter_dict=filter_dict)
-llm_config = {"config_list": config_list, "timeout": 120}
+# Basic configuration for logging
+logging.basicConfig(filename='teachable_agent_session.log', 
+                    level=logging.INFO, 
+                    format='%(asctime)s %(levelname)s:%(message)s')
 
-def create_teachable_agent(reset_db=False, verbosity=0):
-    """Instantiates a TeachableAgent using the settings from the top of this file."""
-    # Load LLM inference endpoints from an env variable or a file
-    # See https://microsoft.github.io/autogen/docs/FAQ#set-your-api-endpoints
-    # and OAI_CONFIG_LIST_sample
-    config_list = config_list_from_json(env_or_file=OAI_CONFIG_LIST, filter_dict=filter_dict, file_location=KEY_LOC)
-    teachable_agent = TeachableAgent(
-        name="teachableagent",
-        llm_config={"config_list": config_list, "timeout": 120, "cache_seed": cache_seed},
-        teach_config={
-            "verbosity": verbosity,
-            "reset_db": reset_db,
-            "path_to_db_dir": "./tmp/teachable_agent_db",
-            "recall_threshold": recall_threshold,
-        },
-    )
-    return teachable_agent
 
 class TeachableAgent(ConversableAgent):
     def __init__(self, name="teachableagent", system_message=None, human_input_mode="NEVER",
                  llm_config=None, analyzer_llm_config=None, teach_config=None, **kwargs):
-        super().__init__(name=name, system_message=system_message or "You are a helpful AI assistant that remembers user teachings from prior chats.",
+        default_system_message = "You are a helpful AI assistant that remembers user teachings from prior chats."
+        super().__init__(name=name, system_message=system_message or default_system_message,
                          human_input_mode=human_input_mode, llm_config=llm_config, **kwargs)
+        
         self._teach_config = teach_config or {}
+        self.llm_config = llm_config
+        self.analyzer_llm_config = analyzer_llm_config or llm_config  # Use llm_config if analyzer_llm_config is None
+        self._initialize_teachable_agent_config()
+        self.memo_store = MemoStore(self.verbosity, self.reset_db, self.path_to_db_dir)
+        self.user_comments = []
+        self.register_reply(Agent, self._generate_teachable_assistant_reply, position=2)
+
+    def _initialize_teachable_agent_config(self):
+        """
+        Initializes the configuration for the teachable agent.
+        """
         self.verbosity = self._teach_config.get("verbosity", 0)
         self.reset_db = self._teach_config.get("reset_db", False)
         self.path_to_db_dir = self._teach_config.get("path_to_db_dir", "./tmp/teachable_agent_db")
         self.prepopulate = self._teach_config.get("prepopulate", True)
         self.recall_threshold = self._teach_config.get("recall_threshold", 1.5)
         self.max_num_retrievals = self._teach_config.get("max_num_retrievals", 10)
-        self.analyzer = TextAnalyzerAgent(llm_config=analyzer_llm_config or llm_config)
+        self.analyzer = TextAnalyzerAgent(llm_config=self.analyzer_llm_config)
         self.memo_store = MemoStore(self.verbosity, self.reset_db, self.path_to_db_dir)
         self.user_comments = []
         self.register_reply(Agent, self._generate_teachable_assistant_reply, position=2)
@@ -90,43 +90,70 @@ class MemoStore:
         self.verbosity = verbosity
         self.reset = reset
         self.path_to_db_dir = path_to_db_dir
+        self.path_to_dict = os.path.join(path_to_db_dir, "uid_text_dict.pkl")
+
+        # Initialize the database client and the vector database
         settings = Settings(anonymized_telemetry=False, allow_reset=True, is_persistent=True, persist_directory=path_to_db_dir)
         self.db_client = chromadb.Client(settings)
         self.vec_db = self.db_client.create_collection("memos", get_or_create=True)
+
+        # Handle database reset if required
         if reset:
             self.reset_db()
-        self.path_to_dict = os.path.join(path_to_db_dir, "uid_text_dict.pkl")
-        self.uid_text_dict = {}
-        self.last_memo_id = 0
-        if not reset and os.path.exists(self.path_to_dict):
-            with open(self.path_to_dict, "rb") as f:
-                self.uid_text_dict = pickle.load(f)
-                self.last_memo_id = len(self.uid_text_dict)
+
+        # Load existing data from disk if not resetting
+        self.uid_text_dict = self._load_data_from_disk() if os.path.exists(self.path_to_dict) and not reset else {}
+        self.last_memo_id = len(self.uid_text_dict)
+
+    def _load_data_from_disk(self):
+        """Load memo data from disk."""
+        with open(self.path_to_dict, "rb") as f:
+            return pickle.load(f)
 
     def add_input_output_pair(self, input_text, output_text):
+        """Add a new input-output pair to the database."""
         self.last_memo_id += 1
         self.vec_db.add([input_text], [str(self.last_memo_id)])
         self.uid_text_dict[str(self.last_memo_id)] = (input_text, output_text)
+        self._save_data_to_disk()
 
     def get_related_memos(self, query_text, n_results, threshold):
+        """Retrieve related memos based on a query text."""
         results = self.vec_db.query([query_text], n_results)
-        return [(self.uid_text_dict[uid], output_text, distance) for uid, _, distance in results if distance < threshold]
+        return [(self.uid_text_dict[str(uid)], distance) for uid, _, distance in results if distance < threshold]
 
     def reset_db(self):
+        """Reset the memo database."""
         self.db_client.delete_collection("memos")
-        self.vec_db = self.db_client.create_collection("memos")
+        self.vec_db = self.db_client.create_collection("memos", get_or_create=True)
         self.uid_text_dict = {}
+        self._save_data_to_disk()
 
-def check_agent_response(teachable_agent, user, correct_answer):
-    """Checks whether the agent's response contains the correct answer, and returns the number of errors (1 or 0)."""
+    def _save_data_to_disk(self):
+        """Save memo data to disk."""
+        with open(self.path_to_dict, "wb") as file:
+            pickle.dump(self.uid_text_dict, file)
+
+    def list_memos(self):
+        """Prints all stored memos for debugging purposes."""
+        if self.verbosity >= 1:
+            print("Stored Memos:")
+            for uid, (input_text, output_text) in self.uid_text_dict.items():
+                print(f"ID: {uid}\nInput: {input_text}\nOutput: {output_text}\n")
+
+def check_agent_response(teachable_agent, user, correct_answer, assert_on_error=False):
+    """
+    Checks whether the agent's response contains the correct answer.
+    :param assert_on_error: Set to True to raise an assertion error if the check fails.
+    """
     agent_response = user.last_message(teachable_agent)["content"]
     if correct_answer not in agent_response:
-        print(colored(f"\nTEST FAILED:  EXPECTED ANSWER {correct_answer} NOT FOUND IN AGENT RESPONSE", "light_red"))
+        print(colored(f"\nTEST FAILED: EXPECTED ANSWER {correct_answer} NOT FOUND IN AGENT RESPONSE", "light_red"))
         if assert_on_error:
             assert correct_answer in agent_response
         return 1
     else:
-        print(colored(f"\nTEST PASSED:  EXPECTED ANSWER {correct_answer} FOUND IN AGENT RESPONSE", "light_cyan"))
+        print(colored(f"\nTEST PASSED: EXPECTED ANSWER {correct_answer} FOUND IN AGENT RESPONSE", "light_cyan"))
         return 0
 
 def use_question_answer_phrasing():
@@ -258,24 +285,63 @@ class MemoStore:
                 memos.append((input_text, output_text, distance))
         return memos
 
-def create_teachable_agent(reset_db=False, llm_config=None):
-    return TeachableAgent(llm_config=llm_config, teach_config={"reset_db": reset_db})
+def create_teachable_agent(reset_db=False, verbosity=0):
+    """
+    Instantiates a TeachableAgent with the provided settings.
+    """
+    # Load LLM configurations from the JSON file
+    config_list = config_list_from_json(env_or_file="/Users/lenox27/AI/TraderBuddy/OAI_CONFIG_LIST")
+    llm_config = {"config_list": config_list, "timeout": 120}
+    analyzer_llm_config = llm_config  # For now, use the same config for the analyzer
+
+    teachable_agent = TeachableAgent(
+        llm_config=llm_config,
+        analyzer_llm_config=analyzer_llm_config,
+        teach_config={
+            "verbosity": verbosity,
+            "reset_db": reset_db,
+            "path_to_db_dir": "./tmp/teachable_agent_db",
+            "recall_threshold": 1.5,
+        }
+    )
+    return teachable_agent
 
 def initiate_interactive_session(teachable_agent):
     user = ConversableAgent("user", human_input_mode="ALWAYS")
-    print("Starting interactive session. Type 'exit' to end.")
+    print("Interactive session with Teachable Agent. Type 'exit' to end, 'help' for assistance.")
+
     while True:
-        user_input = input("You: ")
+        user_input = input("You: ").strip()
+
         if user_input.lower() == 'exit':
+            print("Session ending, processing feedback.")
             break
-        response = teachable_agent.handle_query(user_input)
-        print("Agent:", response)
+        elif user_input.lower() == 'help':
+            print("Ask anything or teach me. Type 'exit' to end the session.")
+            continue
+
+        try:
+            # Ensure the original case of user input is preserved for the agent
+            messages = [{"role": "user", "content": user_input}]
+            _, response = teachable_agent._generate_teachable_assistant_reply(messages)
+            print("Agent:", response)
+        except Exception as e:
+            logging.error(f"Session error: {e}, Input: {user_input}")
+            print(f"Error: {e}")
+
+
+def main():
+    try:
+        teachable_agent = create_teachable_agent()
+        initiate_interactive_session(teachable_agent)
+    finally:
+        if 'teachable_agent' in locals():
+            if hasattr(teachable_agent, 'learn_from_user_feedback'):
+                teachable_agent.learn_from_user_feedback()
+            if hasattr(teachable_agent, 'close_db'):
+                teachable_agent.close_db()
+                print("Database closed.")
 
 if __name__ == "__main__":
-    # Create the Teachable Agent
-    teachable_agent = create_teachable_agent()
-    # Start an interactive session
-    initiate_interactive_session(teachable_agent)
-    # Learn from user feedback and close the database
-    teachable_agent.learn_from_user_feedback()
-    teachable_agent.close_db()
+    main()
+
